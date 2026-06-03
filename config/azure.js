7aada33a6e454f04166f27bdbed40773cc106208
DesarrolloInduma <practicante.infra@Induma.com.co>
2026-06-03 10:42:41 -0500

    borre console.log

--- config/azure.js
+++ config/azure.js
@@ -1,14 +1,5 @@
 require("dotenv").config();
 
-console.log("CLIENT ID:");
-console.log(process.env.OUTLOOK_CLIENT_ID);
-
-console.log("TENANT ID:");
-console.log(process.env.OUTLOOK_TENANT_ID);
-
-console.log("CLIENT SECRET:");
-console.log(process.env.OUTLOOK_CLIENT_SECRET);
-
 const msal = require("@azure/msal-node");
 
 const msalConfig = {
